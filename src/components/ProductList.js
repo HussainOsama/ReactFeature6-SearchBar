5a8632323db7ee667d90cf24a446e5f0d2f09140
HussainOsama <hussain.osama.asd@gmail.com>
2021-02-23 14:03:39 +0300

    Done

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,14 +6,20 @@ import SearchBar from "./SearchBar";
 // Data
 import products from "../products";
 
+import { useState } from "react";
+
 const ProductList = () => {
-  const productList = products.map((product) => (
-    <ProductItem product={product} key={product.id} />
-  ));
+  const [query, setQuery] = useState("");
+
+  const productList = products
+    .filter((product) => product.name.includes(query))
+    .map((product) => <ProductItem product={product} key={product.id} />);
+
+  // console.log(query, filterd);
 
   return (
     <>
-      <SearchBar />
+      <SearchBar setQuery={setQuery} />
       <ListWrapper>{productList}</ListWrapper>
     </>
   );
